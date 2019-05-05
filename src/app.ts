dbbc1f5697308a4af4820f457e29145a988186f1
Justin Baker <JustinRBaker88@gmail.com>
2019-05-05 12:25:47 -0700

    updating config files.

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import "phaser";
+import { GameScene } from "GameScene";
+
+const config: GameConfig = {
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  scene: [GameScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: "#000033"
+};
+
+export class StarfallGame extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
+
+window.onload = () => {
+  var game = new StarfallGame(config);
+};
