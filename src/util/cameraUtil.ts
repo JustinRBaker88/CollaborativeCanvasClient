9eed31c607d25029e380ec339080961361ff7d7e
Justin Baker <JustinRBaker88@gmail.com>
2019-06-12 15:28:39 -0700

    Modularizing Camera utuilties.

---
+++ src/util/cameraUtil.ts
@@ -0,0 +1,13 @@
+import "phaser";
+
+export class CameraUtil {
+
+  static readonly MINWIDTH = 1;
+  static readonly MAXWIDTH = 3840;
+
+  public static setCameraWidth(camera : Phaser.Cameras.Scene2D.Camera, pixels :number) : void {
+    pixels = Phaser.Math.Clamp(pixels,this.MINWIDTH,this.MAXWIDTH); 
+    camera.setZoom(camera.width/pixels);
+  }
+
+};
