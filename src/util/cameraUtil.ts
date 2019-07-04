8bb1be002904c0507cc92696e44a29611042b972
Justin Baker <JustinRBaker88@gmail.com>
2019-07-03 19:29:57 -0700

    ultility methods for manipulating the camera.

--- src/util/cameraUtil.ts
+++ src/util/cameraUtil.ts
@@ -6,7 +6,7 @@ export class CameraUtil {
   static readonly MAXWIDTH = 3840;
 
   public static setCameraWidth(camera : Phaser.Cameras.Scene2D.Camera, pixels :number) : void {
-    pixels = Phaser.Math.Clamp(pixels,this.MINWIDTH,this.MAXWIDTH); 
+    pixels = Phaser.Math.Clamp(pixels,this.MINWIDTH,this.MAXWIDTH);
     camera.setZoom(camera.width/pixels);
   }
 
