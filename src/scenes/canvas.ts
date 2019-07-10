e2bd0e464178966241c16651e8e3da1e7a2c0195
Justin Baker <JustinRBaker88@gmail.com>
2019-07-09 19:19:30 -0700

    changed zooming from linear to exponential.

--- src/scenes/canvas.ts
+++ src/scenes/canvas.ts
@@ -1,14 +1,23 @@
 import "phaser";
 import { CanvasTile } from '../gameobjects/tile';
 import { CameraUtil } from '../util/cameraUtil';
+import { CameraDragController } from '../controllerobjects/cameraDragController';
 
 export class Canvas extends Phaser.Scene {
 
   canvasGroup: Array<Array<CanvasTile>>;
   mCamera: Phaser.Cameras.Scene2D.Camera;
 
-  readonly CANVASWIDTH: number = 20;
-  readonly CANVASHEIGHT: number = 20;
+  cameraDragController : CameraDragController;
+
+  minZoom: number = 5;
+  maxZoom: number = 70;
+
+  zoomPercent : number;
+
+
+  readonly CANVASWIDTH: number = 100;
+  readonly CANVASHEIGHT: number = 100;
 
 
   constructor() {
@@ -28,10 +37,14 @@ export class Canvas extends Phaser.Scene {
   create(startData: Object ): void {
     this.initCamera();
     this.initCanvas();
-    this.input.on(Phaser.Input.Events.POINTER_WHEEL, this.scrollWheelHandler, this);
+    this.initEvents();
+
+    this.cameraDragController = new CameraDragController(this.mCamera, this); 
+
   }
 
   update(time: number, delta: number): void {
+    this.cameraDragController.update(delta);
   }
 
   private initCanvas() {
@@ -47,41 +60,62 @@ export class Canvas extends Phaser.Scene {
       }
       this.canvasGroup.push(row);
     }
+  }
+
+  private initEvents() : void {
     this.input.on(Phaser.Input.Events.POINTER_DOWN, this.pointerDownHandler, this);
+    this.input.on(Phaser.Input.Events.POINTER_WHEEL, this.scrollWheelHandler, this);
   }
 
   private initCamera() : void {
     this.mCamera = this.cameras.main;
 
     CameraUtil.setCameraWidth(this.mCamera,this.CANVASWIDTH);
-    this.mCamera.setBounds(0,0,this.CANVASWIDTH + 1,this.CANVASHEIGHT + 1);
-    this.mCamera.centerOn(5,5);
-    this.input.on(Phaser.Input.Events.DRAG, this.dragHandler, this);
+    this.mCamera.centerOn(50,50);
+    this.mCamera.setBounds(0, 0, 100,100);
+    this.zoomPercent = this.zoomValueToPercent(this.mCamera.zoom);
+    console.log("Starting percent zoom: " + this.zoomPercent);
   }
 
   private pointerDownHandler(pointer : Phaser.Input.Pointer) {
     if (pointer.primaryDown) {
+
       let coordinates : Phaser.Math.Vector2 = new Phaser.Math.Vector2(); 
       pointer.positionToCamera(this.mCamera, coordinates);
       const x: number = Math.floor(pointer.worldX);
       const y: number = Math.floor(pointer.worldY);
-      let tile = this.canvasGroup[x][y] as CanvasTile;
-      tile.randomizeColor();
+
+      if (x < this.CANVASWIDTH && y < this.CANVASHEIGHT) {
+        let tile = this.canvasGroup[x][y] as CanvasTile;
+        if (tile != null) {
+          tile.randomizeColor();
+        }
+      }
     }
   }
 
-  private dragHandler(pointer : Phaser.Input.Pointer) {
-    console.log("In drag event handler");
-    this.mCamera.setScroll(this.mCamera.x + pointer.deltaX, this.mCamera.y + pointer.deltaY);
+  private zoomPercentToValue(percent : number) : number {
+    return this.minZoom * Math.exp(percent*(Math.log(this.maxZoom/this.minZoom)/100));
+  }
+
+  private zoomValueToPercent(zoom : number) : number {
+    return (100*Math.log(zoom/this.minZoom))/Math.log(this.maxZoom/this.minZoom);
   }
 
   private scrollWheelHandler(pointer : Phaser.Input.Pointer) {
+    const deltaZoom = Math.abs(pointer.deltaY);
+    let percent = this.zoomValueToPercent(this.mCamera.zoom);
 
+    // mouse wheel down, zoom out
     if (pointer.deltaY > 0) {
-      this.mCamera.zoom -= 1;
+      percent = Phaser.Math.Clamp(percent - deltaZoom, 0 , 100);
     }
+    // mouse wheel up, zoom in
     else if (pointer.deltaY < 0) {
-      this.mCamera.zoom += 1;
+      percent = Phaser.Math.Clamp(percent + deltaZoom, 0 , 100);
     }
+
+    this.mCamera.setZoom(this.zoomPercentToValue(percent));
+    this.zoomPercent = percent;
   }
 };
