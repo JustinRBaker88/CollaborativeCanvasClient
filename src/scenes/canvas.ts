f13964494eeeb6e5eba48fd90db0c77dbc4efc29
Justin Baker <JustinRBaker88@gmail.com>
2019-07-12 23:19:01 -0700

    Added optional debug text & fixed negative tile bounds issue.

--- src/scenes/canvas.ts
+++ src/scenes/canvas.ts
@@ -12,8 +12,7 @@ export class Canvas extends Phaser.Scene {
   cameraDragController : CameraDragController;
   cameraZoomController : CameraZoomController;
 
-  minZoom: number = 5;
-  maxZoom: number = 70;
+  debugText : Phaser.GameObjects.Text;
 
   readonly CANVASWIDTH: number = 100;
   readonly CANVASHEIGHT: number = 100;
@@ -34,6 +33,8 @@ export class Canvas extends Phaser.Scene {
   }
 
   create(startData: Object ): void {
+    
+    
     this.initCamera();
     this.initCanvas();
     this.initEvents();
@@ -41,10 +42,22 @@ export class Canvas extends Phaser.Scene {
     this.cameraDragController = new CameraDragController(this.mCamera, this);
     this.cameraZoomController = new CameraZoomController(this.mCamera, this);
 
+    // this.debugText = this.scene.get("CanvasUI").add.text(0, 0, "test").setScrollFactor(0);
+    // this.debugText.setShadow(1, 1, '#000000', 2);
+
+    // this.debugText.scale = 3;
   }
 
   update(time: number, delta: number): void {
     this.cameraDragController.update(delta);
+
+    // this.debugText.setText(['x       : ' + this.mCamera.scrollX.toFixed(1), 
+    //                         'y       : ' + this.mCamera.scrollY.toFixed(1),
+    //                         'zoom    : ' + this.mCamera.zoom.toFixed(1),
+    //                         'midpoint: ' + "[" + this.mCamera.midPoint.x.toFixed(1) +", " + this.mCamera.midPoint.y.toFixed(1),
+    //                         'DspSize : ' + "[" +  this.mCamera.displayWidth.toFixed(1) + ", " + this.mCamera.displayHeight.toFixed(1) + "]",
+    //                         'pointer : ' + "[" + (this.input.x/this.mCamera.width).toFixed(3) + ", " + (this.input.y/this.mCamera.height).toFixed(3) +"]",
+    //                         'wPointer: ' + "[" + this.input.activePointer.worldX.toFixed(1) + ", " +this.input.activePointer.worldY.toFixed(1) +"]"]);
   }
 
   private initCanvas() {
@@ -70,8 +83,8 @@ export class Canvas extends Phaser.Scene {
     this.mCamera = this.cameras.main;
 
     CameraUtil.setCameraWidth(this.mCamera,this.CANVASWIDTH);
-    this.mCamera.centerOn(50,50);
-    this.mCamera.setBounds(-100, -100, 400,400);
+    //this.mCamera.setBounds(-100, -100, 500,500);
+    this.mCamera.centerOn(0,0);
   }
 
   private pointerDownHandler(pointer : Phaser.Input.Pointer) {
@@ -82,7 +95,7 @@ export class Canvas extends Phaser.Scene {
       const x: number = Math.floor(pointer.worldX);
       const y: number = Math.floor(pointer.worldY);
 
-      if (x < this.CANVASWIDTH && y < this.CANVASHEIGHT) {
+      if (x < this.CANVASWIDTH && y < this.CANVASHEIGHT && x >= 0 && y >= 0) {
         let tile = this.canvasGroup[x][y] as CanvasTile;
         if (tile != null) {
           tile.randomizeColor();
