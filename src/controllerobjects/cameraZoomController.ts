dfc60e39d753f0979d3eb75927ca0e8d1324d312
Justin Baker <JustinRBaker88@gmail.com>
2019-07-12 23:17:08 -0700

    Added zoom to cursor functionality.

--- src/controllerobjects/cameraZoomController.ts
+++ src/controllerobjects/cameraZoomController.ts
@@ -4,46 +4,58 @@ export class CameraZoomController {
   targetCamera: Phaser.Cameras.Scene2D.Camera;
   scene: Phaser.Scene;
 
-  minZoom: number = 5;
+  zoomPercent : number = .5;
+
+  minZoom: number = .5;
   maxZoom: number = 70;
 
-  constructor(camera : Phaser.Cameras.Scene2D.Camera, scene: Phaser.Scene, config? : any) {
+  constructor(camera : Phaser.Cameras.Scene2D.Camera, scene: Phaser.Scene, initialZoom?: number, config? : any) {
 
     this.targetCamera = camera;
     this.scene = scene;
+
+    if (initialZoom != null) {
+      this.zoomPercent = initialZoom;
+    }
+    this.setZoomPercent(this.zoomPercent);
 
     this.initEventListeners();
 
     // TODO: implement Config object and config initilization method
   }
 
+  private zoomFromPercent(percent : number) : number {
+    return this.minZoom + (this.maxZoom-this.minZoom)*Phaser.Math.Easing.Expo.In(this.zoomPercent);
+  }
+
   private initEventListeners() {
     this.scene.input.on(Phaser.Input.Events.POINTER_WHEEL, this.scrollWheelHandler, this);
   }
 
-
-  private zoomPercentToValue(percent : number) : number {
-    return this.minZoom * Math.exp(percent*(Math.log(this.maxZoom/this.minZoom)/100));
-  }
-
-  private zoomValueToPercent(zoom : number) : number {
-    return (100*Math.log(zoom/this.minZoom))/Math.log(this.maxZoom/this.minZoom);
+  public setZoomPercent(percent: number) {
+    this.zoomPercent = Phaser.Math.Clamp(percent, 0, 100);
+    this.targetCamera.setZoom(this.zoomFromPercent(this.zoomPercent));
   }
 
   private scrollWheelHandler(pointer : Phaser.Input.Pointer) {
-    const deltaZoom = Math.abs(pointer.deltaY);
-    let percent = this.zoomValueToPercent(this.targetCamera.zoom);
+    // Relative position of pointer at time of zoom event
+    const pointerPercentX : number = (pointer.x / this.targetCamera.width);
+    const pointerPercentY : number = (pointer.y / this.targetCamera.height);
+    
+    const deltaZoom = (pointer.deltaY * -1)/100;
+    this.zoomPercent = Phaser.Math.Clamp(this.zoomPercent + deltaZoom ,0,1);
 
-    // mouse wheel down, zoom out
-    if (pointer.deltaY > 0) {
-      percent = Phaser.Math.Clamp(percent - deltaZoom, 0 , 100);
-    }
-    // mouse wheel up, zoom in
-    else if (pointer.deltaY < 0) {
-      percent = Phaser.Math.Clamp(percent + deltaZoom, 0 , 100);
-    }
+    const oldDisplayWidth : number = this.targetCamera.displayWidth;
+    const oldDisplayHeight : number = this.targetCamera.displayHeight;
 
-    this.targetCamera.setZoom(this.zoomPercentToValue(percent));
+    this.targetCamera.setZoom(this.zoomFromPercent(this.zoomPercent));
+
+    const newDisplayWidth : number = this.targetCamera.displayWidth;
+    const newDisplayHeight : number = this.targetCamera.displayHeight;
+
+    this.targetCamera.scrollX -= (pointerPercentX-.5)*(newDisplayWidth - oldDisplayWidth);
+    this.targetCamera.scrollY -= (pointerPercentY-.5)*(newDisplayHeight - oldDisplayHeight);
+
   }
 
   
