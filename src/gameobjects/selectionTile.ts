875c4744228cbfdfa415f637d5cb0dfc80b6f9a4
Justin Baker <JustinRBaker88@gmail.com>
2019-07-17 18:36:51 -0700

    Class to represent current user tile selection.

--- src/gameobjects/selectionTile.ts
+++ src/gameobjects/selectionTile.ts
@@ -1,22 +1,75 @@
 import "phaser";
-import { Colors } from '../util/Colors';
+import { ColorMap } from '../util/Colors';
 
-const values: number[] = Colors.getArray();
+export class SelectionTile {
 
-export class selectionTile extends Phaser.GameObjects.Rectangle {
+  private scene : Phaser.Scene;
+  private pointer : Phaser.Input.Pointer;
+  private graphic : Phaser.GameObjects.Graphics;
+  private innerRect : Phaser.Geom.Rectangle;
+  private outterRect : Phaser.Geom.Rectangle;
 
-  constructor(scene: Phaser.Scene, x: number, y:number, color?: Phaser.Display.Color ) {
-    const numericColor : number = Colors.get("WHITE");
+  private color: number;
+
+  private cancelKey : Phaser.Input.Keyboard.Key;
+
+  constructor(scene: Phaser.Scene, pointer: Phaser.Input.Pointer, color: number ) {
+    this.scene = scene;
+    this.pointer = pointer;
     
-    super(scene, x, y, 1, 1, color == null ?  numericColor : color.color);
-  
+    this.graphic = this.scene.add.graphics();
+    this.initGraphics();
+    this.setColor(color);
+    this.enableTile(true);
+
+    // event listeners
+    this.scene.events.on("CameraZoomEvent", this.cameraZoomHandler, this);
+    this.cancelKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+    this.cancelKey.on('up', this.selectionCancelHandler, this);
+    this.scene.events.on("colorSelected", this.colorSelectionHandler, this);
+
+  }
+
+  public enableTile(enabled : boolean) {
+    this.graphic.visible = enabled;
+
+  }
+
+  public update(time: number, delta: number) {
+    if (this.graphic.visible) {
+      this.graphic.setPosition(Math.floor(this.pointer.worldX), Math.floor(this.pointer.worldY));
+   }
+  }
+
+  private colorSelectionHandler(colorKey : string) {
+    this.setColor(ColorMap.get(colorKey));
+    this.enableTile(true);
+  }
+
+  private selectionCancelHandler(key: Phaser.Input.Keyboard.Key, event: KeyboardEvent) {
+    this.enableTile(false);
+  }
+
+  private cameraZoomHandler(pointer : Phaser.Input.Pointer) {
+    this.graphic.setPosition(Math.floor(this.pointer.worldX), Math.floor(this.pointer.worldY));
+  }
+
+
+  private initGraphics() {
+    this.innerRect = new Phaser.Geom.Rectangle(0,0,10,10);
+    this.outterRect = new Phaser.Geom.Rectangle(0.5,0.5,9,9);
+    this.graphic.lineStyle(1, ColorMap.get("Black"));
+    this.graphic.scale = .1;
   }
 
   public setColor(color : number) {
-    this.setFillStyle(color);
+    this.graphic.fillStyle(color);
+    this.graphic.fillRectShape(this.innerRect);
+    this.graphic.strokeRectShape(this.outterRect);
+    this.color = color;
   }
 
-  public randomizeColor() {
-    this.setColor(values[Math.floor(Math.random() * Math.floor(values.length))]);
+  public getColor() : number {
+    return this.color;
   }
 };
